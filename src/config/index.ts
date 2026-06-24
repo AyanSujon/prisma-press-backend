aefe045d5bfb3882cc860e8d3811ca157f6cac2c
Ayan <sujon.seo.bd@gmail.com>
2026-06-24 16:27:39 +0600

    initialized project

---
+++ src/config/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+    path: path.join(process.cwd(), ".env"),
+});
+
+export default {
+    port: process.env.PORT || 5000,
+    databaseUrl: process.env.DATABASE_URL,
+    appUrl: process.env.APP_URL || "http://localhost:5000",
+    bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS || 10,
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+    jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+    jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+
+}
