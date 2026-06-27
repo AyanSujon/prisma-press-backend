17112725003762c9bebdee562c757a115a61a3a3
Ayan <sujon.seo.bd@gmail.com>
2026-06-27 21:19:13 +0600

    added jwt token

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,9 +10,9 @@ export default {
     databaseUrl: process.env.DATABASE_URL,
     appUrl: process.env.APP_URL || "http://localhost:5000",
     bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS || 10,
-    jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
-    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-    jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-    jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET!,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET!,
+    jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN!,
+    jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN!,
 
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface"
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
 
 
 
-const loginUser = async(payload: ILoginUser)=>{
-    const {email, password}= payload; 
+
+const loginUser = async (payload: ILoginUser) => {
+    const { email, password } = payload;
 
     // const user = await prisma.user.findUnique({
     //     where: {email}
@@ -17,20 +20,50 @@ const loginUser = async(payload: ILoginUser)=>{
     // findUnique alternative function for prisma 
 
     const user = await prisma.user.findUniqueOrThrow({
-        where: {email}
+        where: { email }
     })
 
     const isPasswordMatched = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordMatched){
+    if (!isPasswordMatched) {
         throw new Error("Password is incorrect.")
     }
-    return user; 
+
+
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+    const accessToken = jwt.sign(
+        jwtPayload, 
+        config.jwtAccessSecret, 
+        {
+            expiresIn: config.jwtAccessExpiresIn
+        } as SignOptions
+    );
+
+
+    const refreshToken = jwt.sign(
+        jwtPayload, 
+        config.jwtRefreshSecret, 
+        {
+            expiresIn: config.jwtRefreshExpiresIn
+        } as SignOptions
+    );
+
+
+
+    return {
+        accessToken,
+        refreshToken
+    };
 }
 
 
 
-export const authService ={
+export const authService = {
     loginUser,
 
 }
