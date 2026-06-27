789a6a464e79472c1ffd844f93178a64ed81a33e
Ayan <sujon.seo.bd@gmail.com>
2026-06-27 10:58:19 +0600

    implemented catchAsync higher order function for requesthandler

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,32 +1,51 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import HttpStatus from "http-status";
 import { userService } from "./user.service";
+import { NextFunction } from "express-serve-static-core";
+import { catchAsync } from "../../utils/catchAsync";
 
-const registerUser = async(req: Request, res: Response)=>{
-    try{
-    const payload = req.body;
-    const user = await userService.registerUserIntoDB(payload);
-    
-    res.status(HttpStatus.CREATED).json({
-        success: true,
-        statusCode: HttpStatus.CREATED,
-        message: "user Registered Successfully.",
-        data: {
-            user
-        }
-    })
 
-    }catch (error){
-        console.log(error);
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: "failed to register user",
-        error: (error as Error).message
-    })       
-    }
-}
 
+
+// const registerUser = async (req: Request, res: Response) => {
+//     try {
+//         const payload = req.body;
+//         const user = await userService.registerUserIntoDB(payload);
+
+//         res.status(HttpStatus.CREATED).json({
+//             success: true,
+//             statusCode: HttpStatus.CREATED,
+//             message: "user Registered Successfully.",
+//             data: {
+//                 user
+//             }
+//         })
+
+//     } catch (error) {
+//         console.log(error);
+//         res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+//             success: false,
+//             statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+//             message: "failed to register user",
+//             error: (error as Error).message
+//         })
+//     }
+// }
+
+
+const registerUser = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
+        const payload = req.body;
+        const user = await userService.registerUserIntoDB(payload);
+
+        res.status(HttpStatus.CREATED).json({
+            success: true,
+            statusCode: HttpStatus.CREATED,
+            message: "user Registered Successfully.",
+            data: {
+                user
+            }
+        })
+})
 
 export const userController = {
     registerUser,
